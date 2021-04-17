ebd3cbdc0634f4e9f4932ddbc3c0f8708f2677e0
John Leonardo <j@jdleo.me>
2021-04-17 17:33:15 -0500

    completed problem 1512

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 5
+### # Of Problems Solved: 6
--- solutions/1512/main.go
+++ solutions/1512/main.go
@@ -1,1 +1,17 @@
 package main
+
+func numIdenticalPairs(nums []int) int {
+	hm := map[int]int{}
+	res := 0
+
+	for _, num := range nums {
+		if val, ok := hm[num]; ok {
+			res += val
+			hm[num]++
+		} else {
+			hm[num] = 1
+		}
+	}
+
+	return res
+}
