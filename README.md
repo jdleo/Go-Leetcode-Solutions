b5a57d2fbe53ea4c742699004760b5c0b901da1c
John Leonardo <j@jdleo.me>
2021-04-17 17:27:06 -0500

    completed problem 1470

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 4
+### # Of Problems Solved: 5
--- solutions/1470/main.go
+++ solutions/1470/main.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func shuffle(nums []int, n int) []int {
+	res := make([]int, len(nums))
+
+	for i := 0; i < n; i++ {
+		res[2*i] = nums[i]
+		res[2*i+1] = nums[n+i]
+		fmt.Println(2*i, 2*i+1, nums[i], nums[n+i])
+	}
+
+	return res
+}
