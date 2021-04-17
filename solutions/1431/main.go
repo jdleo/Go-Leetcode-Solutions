830b4b1c7007d0b3cb14178303227e43aa8fc91c
John Leonardo <j@jdleo.me>
2021-04-17 17:19:51 -0500

    completed problem 1431

--- solutions/1431/main.go
+++ solutions/1431/main.go
@@ -1,1 +1,18 @@
 package main
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	res := make([]bool, len(candies))
+	max := 0
+
+	for _, candy := range candies {
+		if candy > max {
+			max = candy
+		}
+	}
+
+	for i := 0; i < len(candies); i++ {
+		res[i] = candies[i]+extraCandies >= max
+	}
+
+	return res
+}
