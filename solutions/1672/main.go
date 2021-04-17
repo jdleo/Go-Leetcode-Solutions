fb9861d74b8b75133f4fc478b323774c2d35c35a
John Leonardo <j@jdleo.me>
2021-04-17 17:15:54 -0500

    completed problem 1672

--- solutions/1672/main.go
+++ solutions/1672/main.go
@@ -1,1 +1,17 @@
 package main
+
+func maximumWealth(accounts [][]int) int {
+	res := 0
+
+	for _, account := range accounts {
+		sum := 0
+		for _, bal := range account {
+			sum += bal
+		}
+		if sum > res {
+			res = sum
+		}
+	}
+
+	return res
+}
