52b47914e581cd324685c624170596bdc57554ab
john leonardo <j@jdleo.me>
2021-04-20 10:14:16 -0500

    completed problem 1476

--- solutions/1476/main.go
+++ solutions/1476/main.go
@@ -1,1 +1,17 @@
 package main
+
+type SubrectangleQueries struct {
+	rectangle [][]int
+}
+
+func Constructor(rectangle [][]int) SubrectangleQueries {
+	return SubrectangleQueries{rectangle: rectangle}
+}
+
+func (this *SubrectangleQueries) UpdateSubrectangle(row1 int, col1 int, row2 int, col2 int, newValue int) {
+
+}
+
+func (this *SubrectangleQueries) GetValue(row int, col int) int {
+
+}
