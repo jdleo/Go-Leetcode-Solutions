b62aa8c85082b2bcf88e3664477ecc7f698bcef5
john leonardo <j@jdleo.me>
2021-04-20 10:45:03 -0500

    completed problem 1832

--- solutions/1832/main.go
+++ solutions/1832/main.go
@@ -1,1 +1,15 @@
 package main
+
+func checkIfPangram(sentence string) bool {
+	seen := make([]bool, 26)
+	res := 26
+
+	for _, char := range sentence {
+		if !seen[char-'a'] {
+			res--
+		}
+		seen[char-'a'] = true
+	}
+
+	return res == 0
+}
