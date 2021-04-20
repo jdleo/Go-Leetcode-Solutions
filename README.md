fd9b785674f61130cb3639debc52d93425576fa6
john leonardo <j@jdleo.me>
2021-04-20 10:37:47 -0500

    completed problem 1828

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 6
+### # Of Problems Solved: 7
--- solutions/1828/main.go
+++ solutions/1828/main.go
@@ -1,1 +1,22 @@
 package main
+
+import "math"
+
+func countPoints(points [][]int, queries [][]int) []int {
+	res := 0
+
+	for _, query := range queries {
+		x1, y1, r := query[0], query[1], float64(query[2])
+
+		for _, point := range points {
+			x2, y2 := point[0], point[1]
+
+			if math.Sqrt(float64(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)))) <= float64(r) {
+				res++
+				break
+			}
+		}
+	}
+
+	return res
+}
