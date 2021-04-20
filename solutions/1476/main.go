e9196d1416109f3e467e5f363e10ce5828c040db
john leonardo <j@jdleo.me>
2021-04-20 10:14:31 -0500

    "create problem 1828"

--- solutions/1476/main.go
+++ solutions/1476/main.go
@@ -9,9 +9,13 @@ func Constructor(rectangle [][]int) SubrectangleQueries {
 }
 
 func (this *SubrectangleQueries) UpdateSubrectangle(row1 int, col1 int, row2 int, col2 int, newValue int) {
-
+	for i := row1; i <= row2; i++ {
+		for j := col1; j <= col2; j++ {
+			this.rectangle[i][j] = newValue
+		}
+	}
 }
 
 func (this *SubrectangleQueries) GetValue(row int, col int) int {
-
+	return this.rectangle[row][col]
 }
