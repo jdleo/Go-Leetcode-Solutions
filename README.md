1c381b28ac4f23992230a7474e22834843d6aab8
john leonardo <j@jdleo.me>
2021-05-03 17:06:54 -0500

    completed problem 1221

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 21
+### # Of Problems Solved: 22
--- solutions/1221/main.go
+++ solutions/1221/main.go
@@ -1,1 +1,23 @@
 package main
+
+func balancedStringSplit(s string) int {
+	res, bal := 0, 0
+
+	// go through string
+	for _, char := range s {
+		// adjust balance based on L/R characters
+		switch char {
+		case 'L':
+			bal++
+		case 'R':
+			bal--
+		}
+
+		// check if evenly balanced
+		if bal == 0 {
+			res++
+		}
+	}
+
+	return res
+}
