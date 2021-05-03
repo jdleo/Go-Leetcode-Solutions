54370b4984fd1729579def37cd87e8999c605041
john leonardo <j@jdleo.me>
2021-05-03 16:09:28 -0500

    completed problem 1528

--- solutions/1528/main.go
+++ solutions/1528/main.go
@@ -1,1 +1,15 @@
 package main
+
+func restoreString(s string, indices []int) string {
+	// result byte slice
+	res := make([]byte, len(s))
+
+	// go through string
+	for i := 0; i < len(s); i++ {
+		// insert byte i, at indices i
+		res[indices[i]] = s[i]
+	}
+
+	// result as string
+	return string(res)
+}
