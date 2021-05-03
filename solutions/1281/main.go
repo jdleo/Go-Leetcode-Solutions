cd9e1f24b137d450619b035ea3017d5434d31ffa
john leonardo <j@jdleo.me>
2021-05-03 16:14:07 -0500

    completed problem 1281

--- solutions/1281/main.go
+++ solutions/1281/main.go
@@ -1,1 +1,17 @@
 package main
+
+func subtractProductAndSum(n int) int {
+	// product and sum
+	product, sum := 1, 0
+
+	// go until no more digits
+	for n > 0 {
+		// multiply by rightmost digit, add rightmost digit
+		product *= n % 10
+		sum += n % 10
+		// shift to get next digit
+		n /= 10
+	}
+
+	return product - sum
+}
