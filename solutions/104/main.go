74dbad9a33edff277a0f296004ab8bae79086c17
John Leonardo <j@jdleo.me>
2021-05-03 21:23:05 -0500

    completed problem 104

--- solutions/104/main.go
+++ solutions/104/main.go
@@ -1,1 +1,28 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	// base case
+	if root == nil {
+		return 0
+	}
+
+	// get left and right subtree depths
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	// check max
+	if left > right {
+		// count current depth + left
+		return 1 + left
+	} else {
+		// count current depth + right
+		return 1 + right
+	}
+}
