58669f221a223cda3782dc6f87e4995752369294
john leonardo <j@jdleo.me>
2021-05-04 13:45:26 -0500

    completed problem 169

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 39
+### # Of Problems Solved: 40
--- solutions/169/main.go
+++ solutions/169/main.go
@@ -1,1 +1,24 @@
 package main
+
+func majorityElement(nums []int) int {
+	// majority element, and count
+	major, count := nums[0], 1
+
+	// go through rest of nums
+	for i := 1; i < len(nums); i++ {
+		// check if current streak is 0
+		if count == 0 {
+			// increment and set majority
+			count++
+			major = nums[i]
+		} else if major == nums[i] {
+			// keep streak going
+			count++
+		} else {
+			// decrease streak
+			count--
+		}
+	}
+
+	return major
+}
