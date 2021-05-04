387b5c33115466987800727aa3da5e736d30c2cb
john leonardo <j@jdleo.me>
2021-05-04 13:30:57 -0500

    completed problem 238

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 37
+### # Of Problems Solved: 38
--- solutions/238/main.go
+++ solutions/238/main.go
@@ -1,1 +1,23 @@
 package main
+
+func productExceptSelf(nums []int) []int {
+	// left and right running products
+	left, right := 1, 1
+	// result array filled with 1s
+	res := make([]int, len(nums))
+	for i := 0; i < len(res); i++ {
+		res[i] = 1
+	}
+
+	// iterate thru nums length
+	for i := 0; i < len(nums); i++ {
+		// multiply into to res the current left and right products
+		res[i] *= left
+		res[len(res)-i-1] *= right
+		// multiply into left and right products
+		left *= nums[i]
+		right *= nums[len(res)-i-1]
+	}
+
+	return res
+}
