8fbf01ccf63f1476d97b4f376365e03450b7ad86
john leonardo <j@jdleo.me>
2021-05-04 13:56:45 -0500

    completed problem 48

--- solutions/48/main.go
+++ solutions/48/main.go
@@ -7,11 +7,11 @@ func rotate(matrix [][]int) {
 		matrix[i], matrix[len(matrix)-i-1] = matrix[len(matrix)-i-1], matrix[i]
 	}
 
-	// swap mirrored cells
+	// swap symmetric cells
 	for i := 0; i < len(matrix); i++ {
 		for j := i + 1; j < len(matrix); j++ {
 			// swap cells
-			matrix[i], matrix[j] = matrix[j], matrix[i]
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 		}
 	}
 }
