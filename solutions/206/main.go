5ef9245f763b6b2d69cbafcf6495759777f482db
John Leonardo <j@jdleo.me>
2021-05-03 23:12:27 -0500

    completed problem 206

--- solutions/206/main.go
+++ solutions/206/main.go
@@ -1,1 +1,23 @@
 package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+
+	// go while there is something in head
+	for head != nil {
+		// swap
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+
+	// result will be at prev
+	return prev
+}
