183f64119ce5ce9927532954f453de39040cde6e
john leonardo <j@jdleo.me>
2021-05-04 14:26:06 -0500

    completed problem 287

--- solutions/287/main.go
+++ solutions/287/main.go
@@ -1,1 +1,23 @@
 package main
+
+func findDuplicate(nums []int) int {
+	// tortoise and hare
+	slow := nums[0]
+	fast := nums[nums[0]]
+
+	// go til they meet
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+	}
+
+	// reset fast
+	fast = 0
+	// go til they meet
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+
+	return slow
+}
