c4a7fe21da8479981c5f9d79ef64558201dc3268
john leonardo <j@jdleo.me>
2021-05-04 13:41:55 -0500

    completed problem 48

--- solutions/48/main.go
+++ solutions/48/main.go
@@ -1,1 +1,17 @@
 package main
+
+func rotate(matrix [][]int) {
+	// first, go through and swap individual rows
+	for i := 0; i < len(matrix)/2; i++ {
+		// swap row in front with row in back
+		matrix[i], matrix[len(matrix)-i-1] = matrix[len(matrix)-i-1], matrix[i]
+	}
+
+	// swap mirrored cells
+	for i := 0; i < len(matrix); i++ {
+		for j := i + 1; j < len(matrix); j++ {
+			// swap cells
+			matrix[i], matrix[j] = matrix[j], matrix[i]
+		}
+	}
+}
