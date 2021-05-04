c4b0998b989d55d79b00411dea2c1fb959c2c768
john leonardo <j@jdleo.me>
2021-05-04 13:20:48 -0500

    completed problem 347

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 36
+### # Of Problems Solved: 37
--- solutions/347/main.go
+++ solutions/347/main.go
@@ -1,1 +1,26 @@
 package main
+
+import "sort"
+
+func topKFrequent(nums []int, k int) []int {
+	// map to hold number frequencies
+	counts := map[int]int{}
+	// fill frequencies
+	for _, num := range nums {
+		counts[num]++
+	}
+
+	// keys of frequencies (values)
+	keys := []int{}
+	for k := range counts {
+		keys = append(keys, k)
+	}
+
+	// sort keys based on frequency
+	sort.Slice(keys, func(i int, j int) bool {
+		return counts[keys[i]] > counts[keys[j]]
+	})
+
+	// return k most frequent
+	return keys[:k]
+}
