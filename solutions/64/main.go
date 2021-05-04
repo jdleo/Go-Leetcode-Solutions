95ce45478de062a3e85b9e62147e79722399dcc5
john leonardo <j@jdleo.me>
2021-05-04 14:42:53 -0500

    completed problem 64

--- solutions/64/main.go
+++ solutions/64/main.go
@@ -1,1 +1,21 @@
 package main
+
+func minPathSum(grid [][]int) int {
+	// fill first col, and first row w running sum (only one direction here)
+	for i := 1; i < len(grid); i++ {
+		grid[i][0] += grid[i-1][0]
+	}
+	for j := 1; j < len(grid[0]); j++ {
+		grid[0][j] += grid[0][j-1]
+	}
+
+	// go thru rest of grid
+	for i := 1; i < len(grid); i++ {
+		for j := 1; j < len(grid[0]); j++ {
+
+		}
+	}
+
+	// result is in last cell
+	return grid[len(grid)-1][len(grid[0])-1]
+}
