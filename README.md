29f663f6cef89884fe29ee0ffb99e3f47956934a
john leonardo <j@jdleo.me>
2021-05-05 13:23:49 -0500

    completed problem 11

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 48
+### # Of Problems Solved: 49
--- solutions/11/main.go
+++ solutions/11/main.go
@@ -1,1 +1,24 @@
 package main
+
+func maxArea(height []int) int {
+	res, left, right := 0, 0, len(height)-1
+
+	for left < right {
+		// check the lower wall
+		if height[left] < height[right] {
+			// set new max area, and move left forward
+			if height[left]*(right-left) > res {
+				res = height[left] * (right - left)
+			}
+			left++
+		} else {
+			// set new max area, and move right forward
+			if height[right]*(right-left) > res {
+				res = height[right] * (right - left)
+			}
+			right--
+		}
+	}
+
+	return res
+}
