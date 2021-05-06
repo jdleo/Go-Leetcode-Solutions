7d1ecec1636e1f350ba80b43fa18a03ee05083d7
john leonardo <j@jdleo.me>
2021-05-06 16:44:12 -0500

    completed problem 53

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 66
+### # Of Problems Solved: 67
--- solutions/53/main.go
+++ solutions/53/main.go
@@ -1,1 +1,26 @@
 package main
+
+func maxSubArray(nums []int) int {
+	res := nums[0]
+	// maximum subarray sums at each index
+	maxSums := make([]int, len(nums))
+	maxSums[0] = nums[0]
+
+	// go through nums
+	for i := 1; i < len(nums); i++ {
+		// the max sum at this index, is the max sum of
+		// either 1) continuing sum, starting new sum
+		maxSums[i] = max(maxSums[i-1]+nums[i], nums[i])
+		// new max
+		res = max(res, maxSums[i])
+	}
+
+	return res
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
