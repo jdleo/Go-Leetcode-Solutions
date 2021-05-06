3f8b2b37a2662df23bbbfab542a053567af34419
john leonardo <j@jdleo.me>
2021-05-06 15:10:50 -0500

    completed problem 1650

--- solutions/1650/main.go
+++ solutions/1650/main.go
@@ -1,1 +1,57 @@
 package main
+
+// Definition for Node.
+type Node struct {
+	Val    int
+	Left   *Node
+	Right  *Node
+	Parent *Node
+}
+
+func lowestCommonAncestor(p *Node, q *Node) *Node {
+	// find root of tree
+	root := getRoot(p)
+	// parent mapping and queue for bfs
+	parent := map[*Node]*Node{}
+	queue := []*Node{}
+
+	// go while queue isnt empty
+	for len(queue) > 0 {
+		// offer from queue
+		cur := queue[0]
+		queue = queue[1:]
+
+		if cur != nil {
+			// map children to parent
+			parent[cur.Left] = cur
+			parent[cur.Right] = cur
+			// add children to queue
+			queue = append(queue, cur.Left)
+			queue = append(queue, cur.Right)
+		}
+	}
+
+	// seen set
+	seen := map[*Node]bool{}
+
+	// go through p's path
+	for p != nil {
+		seen[p] = true
+		p = parent[p]
+	}
+
+	// go until we find q in seen (LCA)
+	for !seen[q] {
+		q = parent[q]
+	}
+
+	return q
+}
+
+// helper method to find root
+func getRoot(node *Node) *Node {
+	for node.Parent != nil {
+		node = node.Parent
+	}
+	return node
+}
