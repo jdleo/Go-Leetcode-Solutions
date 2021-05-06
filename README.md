ba942b165462612772b8a3780b457ca023d69ba9
john leonardo <j@jdleo.me>
2021-05-06 16:35:37 -0500

    completed problem 70

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 64
+### # Of Problems Solved: 65
--- solutions/70/main.go
+++ solutions/70/main.go
@@ -1,1 +1,17 @@
 package main
+
+func climbStairs(n int) int {
+	if n <= 1 {
+		return 1
+	}
+
+	// dinstinct ways 1 step ago, 2 steps ago
+	a, b := 1, 1
+
+	// calculate rest
+	for i := 2; i <= n; i++ {
+		a, b = a+b, a
+	}
+
+	return a
+}
