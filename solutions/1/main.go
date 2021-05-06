d604395321e4756b6bcf8ef8952f8f9e342a95cd
john leonardo <j@jdleo.me>
2021-05-06 16:50:40 -0500

    completed problem 1

--- solutions/1/main.go
+++ solutions/1/main.go
@@ -1,1 +1,21 @@
 package main
+
+func twoSum(nums []int, target int) []int {
+	// map of numbers we have seen, with their index
+	seen := map[int]int{}
+
+	// go thru nums
+	for i, num := range nums {
+		// num + x = target
+		// x = target - num
+		if val, ok := seen[target-num]; ok {
+			// found match
+			return []int{val, i}
+		}
+		// write to seen
+		seen[num] = i
+	}
+
+	// never found :o
+	return []int{-1, -1}
+}
