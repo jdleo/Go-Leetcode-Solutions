6af314e3d410406814ebc4be2e985e44a8ca7da0
John Leonardo <j@jdleo.me>
2021-05-08 18:15:17 -0500

    completed problem 3

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 87
+### # Of Problems Solved: 88
--- solutions/3/main.go
+++ solutions/3/main.go
@@ -1,1 +1,27 @@
 package main
+
+func lengthOfLongestSubstring(s string) int {
+	// longest substring
+	res := 0
+	// map to hold characters we've seen
+	seen := map[byte]bool{}
+
+	// go through string with start, end pointers
+	for start, end := 0, 0; end < len(s); end++ {
+		// keep removing from seen, and moving start forward
+		// as so long as we've already seen this char
+		for seen[s[end]] {
+			delete(seen, s[start])
+			start++
+		}
+
+		// ok, now we can add character at end pointer
+		seen[s[end]] = true
+		// set new max length
+		if end-start+1 > res {
+			res = end - start + 1
+		}
+	}
+
+	return res
+}
