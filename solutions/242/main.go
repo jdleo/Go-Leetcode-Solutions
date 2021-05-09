c720b25052121a21de2926357b079077078da380
John Leonardo <j@jdleo.me>
2021-05-09 02:39:15 -0500

    completed problem 242

--- solutions/242/main.go
+++ solutions/242/main.go
@@ -1,1 +1,22 @@
 package main
+
+func isAnagram(s string, t string) bool {
+	// counts of characters
+	counts := make([]int, 26)
+	// fill counts
+	for _, char := range s {
+		counts[char-'a']++
+	}
+	for _, char := range t {
+		counts[char-'a']--
+	}
+
+	// make sure balanced
+	for _, c := range counts {
+		if c != 0 {
+			return false
+		}
+	}
+
+	return true
+}
