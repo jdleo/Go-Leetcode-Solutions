8147669b1a7c33ac0b4b023ebde5a436ccd3840e
John Leonardo <j@jdleo.me>
2021-05-09 23:28:19 -0500

    completed problem 13

--- solutions/13/main.go
+++ solutions/13/main.go
@@ -1,1 +1,28 @@
 package main
+
+import "math"
+
+func romanToInt(s string) int {
+	res, last := 0, math.MaxInt64
+
+	// roman mappings
+	romans := map[rune]int{
+		'I': 1, 'V': 5, 'X': 10,
+		'L': 50, 'C': 100, 'D': 500,
+		'M': 1_000,
+	}
+
+	for _, char := range s {
+		// check if last char value was smaller than cur (ex: IV = 4)
+		if last < romans[char] {
+			// we need to subtract 2*last
+			res -= last * 2
+		}
+		// add this char's roman value
+		res += romans[char]
+		// set last
+		last = romans[char]
+	}
+
+	return res
+}
