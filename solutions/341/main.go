e005fe0e55d26a0f87e9b54079b549a80fbe29de
John Leonardo <j@jdleo.me>
2021-05-09 23:35:14 -0500

    create problem 387

--- solutions/341/main.go
+++ solutions/341/main.go
@@ -1,1 +1,16 @@
 package main
+
+type NestedIterator struct {
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+
+}
+
+func (this *NestedIterator) Next() int {
+
+}
+
+func (this *NestedIterator) HasNext() bool {
+
+}
