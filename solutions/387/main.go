ff4372296979418bbda0d92659c1bcff516c8102
John Leonardo <j@jdleo.me>
2021-05-09 23:38:02 -0500

    completed problem 387

--- solutions/387/main.go
+++ solutions/387/main.go
@@ -1,1 +1,22 @@
 package main
+
+func firstUniqChar(s string) int {
+	// counts of lowercase characters
+	counts := make([]int, 26)
+
+	for _, char := range s {
+		// increment freq
+		counts[char-'a']++
+	}
+
+	// go back thru string
+	for i, char := range s {
+		// see if unique
+		if counts[char-'a'] == 1 {
+			return i
+		}
+	}
+
+	// doesnt exist
+	return -1
+}
