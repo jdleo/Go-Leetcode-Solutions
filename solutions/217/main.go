af8182c08a7e2ecb6833b181c2c8213a49007a41
John Leonardo <j@jdleo.me>
2021-05-09 23:22:42 -0500

    completed problem 217

--- solutions/217/main.go
+++ solutions/217/main.go
@@ -1,1 +1,17 @@
 package main
+
+func containsDuplicate(nums []int) bool {
+	// map to keep track of seen nums
+	seen := map[int]bool{}
+
+	for _, num := range nums {
+		// check if we've seen this num before
+		if seen[num] {
+			return true
+		}
+
+		seen[num] = true
+	}
+
+	return false
+}
