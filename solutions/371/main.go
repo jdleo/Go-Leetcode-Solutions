0acf21d75d7d335d9e7c7fb2ce3cd8cf0be0d50c
John Leonardo <j@jdleo.me>
2021-05-09 23:59:50 -0500

    completed problem 371

--- solutions/371/main.go
+++ solutions/371/main.go
@@ -1,1 +1,15 @@
 package main
+
+func getSum(a int, b int) int {
+	// if b is 0, its just a
+	if b == 0 {
+		return a
+	}
+
+	// get the carry bit
+	carry := (a & b) << 1
+	// sum
+	sum := a ^ b
+	// pass sum and carry
+	return getSum(sum, carry)
+}
