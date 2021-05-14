dec446eccbde8a3a81ce37016cdd72b1e6e53d50
john leonardo <j@jdleo.me>
2021-05-14 17:14:32 -0500

    completed problem 709

--- solutions/709/main.go
+++ solutions/709/main.go
@@ -1,1 +1,20 @@
 package main
+
+func toLowerCase(str string) string {
+	res := make([]byte, len(str))
+
+	// go thru string
+	for i := 0; i < len(str); i++ {
+		// check if uppercase
+		if 65 <= str[i] && str[i] <= 90 {
+			// write this byte + 32
+			res[i] = str[i] + 32
+		} else {
+			// just write this byte
+			res[i] = str[i]
+		}
+	}
+
+	// return byte slice as string
+	return string(res)
+}
