d3885d602c876e6cf28691b94d516dd164743377
john leonardo <j@jdleo.me>
2021-05-14 11:58:44 -0500

    completed problem 1662

--- solutions/1662/main.go
+++ solutions/1662/main.go
@@ -1,1 +1,20 @@
 package main
+
+func arrayStringsAreEqual(word1 []string, word2 []string) bool {
+	// byte slices for words
+	slice1 := []byte{}
+	slice2 := []byte{}
+
+	// fill slice1
+	for _, word := range word1 {
+		slice1 = append(slice1, []byte(word)...)
+	}
+
+	// fill slice2
+	for _, word := range word2 {
+		slice2 = append(slice2, []byte(word)...)
+	}
+
+	// check if string slices are equivalent
+	return string(slice1) == string(slice2)
+}
