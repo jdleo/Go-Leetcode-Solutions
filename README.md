0ad9d7389528ec228ffaea338bcb3d841aa089e7
john leonardo <j@jdleo.me>
2021-05-14 16:55:24 -0500

    completed problem 1290

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 112
+### # Of Problems Solved: 113
--- solutions/1290/main.go
+++ solutions/1290/main.go
@@ -1,1 +1,21 @@
 package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getDecimalValue(head *ListNode) int {
+	res := 0
+
+	// go through linkedlist
+	for head != nil {
+		// shift over res
+		res <<= 1
+		// add this bit to res
+		res |= head.Val
+	}
+
+	return res
+}
