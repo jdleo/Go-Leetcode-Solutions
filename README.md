488e11eb636b7cac57cc4fb35c43ec2464a4b2f6
john leonardo <j@jdleo.me>
2021-05-14 15:56:14 -0500

    completed problem 1684

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 110
+### # Of Problems Solved: 111
--- solutions/1684/main.go
+++ solutions/1684/main.go
@@ -1,1 +1,30 @@
 package main
+
+func countConsistentStrings(allowed string, words []string) int {
+	res := 0
+
+	// allowed character set
+	valid := make([]bool, 26)
+	for _, char := range allowed {
+		valid[char-'a'] = true
+	}
+
+	// go through words
+OUTER:
+	for _, word := range words {
+
+		// go through word
+		for _, char := range word {
+			// check if inconsistent
+			if !valid[char-'a'] {
+				// continue
+				continue OUTER
+			}
+		}
+
+		// no issues
+		res++
+	}
+
+	return res
+}
