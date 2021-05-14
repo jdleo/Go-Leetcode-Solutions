e9dbab71c858245cb311d921d87a07495f64f0ab
john leonardo <j@jdleo.me>
2021-05-14 11:44:02 -0500

    completed problem 938

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 101
+### # Of Problems Solved: 102
--- solutions/938/main.go
+++ solutions/938/main.go
@@ -1,1 +1,27 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	// base case
+	if root == nil {
+		return 0
+	}
+
+	// check if this root is in range
+	if low <= root.Val && root.Val <= high {
+		// we can add this, and also search left, right
+		return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+	} else if root.Val < low {
+		// we only want to search right
+		return rangeSumBST(root.Right, low, high)
+	} else {
+		// we only want to search left
+		return rangeSumBST(root.Left, low, high)
+	}
+}
