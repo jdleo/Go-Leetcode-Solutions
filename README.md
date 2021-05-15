1893f076fb30d3cb3b6ba8f2bc8da457b996d8e3
John Leonardo <j@jdleo.me>
2021-05-15 01:41:05 -0500

    completed problem 1323

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 129
+### # Of Problems Solved: 130
--- solutions/1323/main.go
+++ solutions/1323/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func maximum69Number(num int) int {
+	res, _ := strconv.Atoi(strings.Replace(strconv.Itoa(num), "6", "9", 1))
+	return res
+}
