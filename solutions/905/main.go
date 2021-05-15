51c9b6e5c9026743bae80ddd5930a1cba4ae051d
John Leonardo <j@jdleo.me>
2021-05-15 18:10:08 -0500

    completed problem 905

--- solutions/905/main.go
+++ solutions/905/main.go
@@ -1,1 +1,22 @@
 package main
+
+func sortArrayByParity(nums []int) []int {
+	left, right := 0, len(nums)-1
+
+	// go until ptrs meet
+	for left < right {
+		if nums[left]&1 == 0 {
+			// left is even and in correct spot, keep going
+			left++
+		} else if nums[right]&1 == 1 {
+			// right is odd and in correct spot, keep going
+			right--
+		} else {
+			// they need to be swapped
+			nums[left], nums[right] = nums[right], nums[left]
+			left, right = left+1, right-1
+		}
+	}
+
+	return nums
+}
