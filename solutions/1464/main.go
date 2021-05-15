35b2c27d99abeffb4718e9b3989a4faea32329d4
John Leonardo <j@jdleo.me>
2021-05-15 13:29:04 -0500

    completed problem 1464

--- solutions/1464/main.go
+++ solutions/1464/main.go
@@ -1,1 +1,18 @@
 package main
+
+func maxProduct(nums []int) int {
+	// first and second biggest
+	first, second := -1, -1
+
+	for _, num := range nums {
+		// set first, second biggest
+		if num > first {
+			first, second = num, first
+		} else if num > second {
+			second = num
+		}
+	}
+
+	// first-1 * second-1
+	return (first - 1) * (second - 1)
+}
