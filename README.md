ba33707be65e65651da85521a38bdfebab66a982
John Leonardo <j@jdleo.me>
2021-05-15 13:54:12 -0500

    completed problem 1557

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 142
+### # Of Problems Solved: 143
--- solutions/1557/main.go
+++ solutions/1557/main.go
@@ -1,1 +1,23 @@
 package main
+
+func findSmallestSetOfVertices(n int, edges [][]int) []int {
+	res := []int{}
+
+	// array[i] means that node i has indegree array[i]
+	indegree := make([]bool, n)
+
+	// go through edges
+	for _, edge := range edges {
+		// mark indegree
+		indegree[edge[1]] = true
+	}
+
+	for i := 0; i < n; i++ {
+		// check if this node has indegree of 0
+		if !indegree[i] {
+			res = append(res, i)
+		}
+	}
+
+	return res
+}
