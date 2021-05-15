e22c8f777b3336a68c49a9bb507e62ead8468254
John Leonardo <j@jdleo.me>
2021-05-15 13:38:22 -0500

    completed problem 1374

--- solutions/1374/main.go
+++ solutions/1374/main.go
@@ -1,1 +1,16 @@
 package main
+
+func generateTheString(n int) string {
+	res := make([]byte, n)
+	// check if even (need to add 1 'y')
+	if n&1 == 0 {
+		res[0] = 'y'
+	}
+
+	// start from 1 if it was even, fill rest of characters
+	for i := (n & 1) ^ 1; i < n; i++ {
+		res[i] = 'x'
+	}
+
+	return string(res)
+}
