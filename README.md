29b0f9131bf7110ac814e3130c70569569ffeab9
John Leonardo <j@jdleo.me>
2021-05-15 01:33:39 -0500

    completed problem 832

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 128
+### # Of Problems Solved: 129
--- solutions/832/main.go
+++ solutions/832/main.go
@@ -1,1 +1,10 @@
 package main
+
+func flipAndInvertImage(A [][]int) [][]int {
+	for _, row := range A {
+		for i := 0; i <= (len(row)-1)/2; i++ {
+			row[i], row[len(row)-1-i] = row[len(row)-1-i]^1, row[i]^1
+		}
+	}
+	return A
+}
