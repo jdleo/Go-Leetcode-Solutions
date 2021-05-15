a15699e81daaf7618534a7739e5f8e50c33ef501
John Leonardo <j@jdleo.me>
2021-05-15 12:51:59 -0500

    completed problem 1572

--- solutions/1572/main.go
+++ solutions/1572/main.go
@@ -1,1 +1,17 @@
 package main
+
+func diagonalSum(mat [][]int) int {
+	res, n := 0, len(mat)
+
+	// go thru matrix length
+	for i := 0; i < n; i++ {
+		// add both diagonals
+		res += mat[i][i] + mat[n-i-1][i]
+	}
+
+	// check if odd (that means we double counted middle)
+	if n&1 == 1 {
+		return res - mat[n/2][n/2]
+	}
+	return res
+}
