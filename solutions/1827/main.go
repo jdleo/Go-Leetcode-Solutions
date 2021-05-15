9dd702452f4425aed67bf3134cd3acaf47ec9f1d
John Leonardo <j@jdleo.me>
2021-05-15 01:09:17 -0500

    completed problem 1827

--- solutions/1827/main.go
+++ solutions/1827/main.go
@@ -1,1 +1,16 @@
 package main
+
+func minOperations(nums []int) int {
+	res := 0
+
+	for i := 1; i < len(nums); i++ {
+		// check if this is non-increasing
+		if nums[i] <= nums[i-1] {
+			// we have to increase current num to make it strictly increasing
+			res += nums[i-1] + 1 - nums[i]
+			nums[i] = nums[i-1] + 1
+		}
+	}
+
+	return res
+}
