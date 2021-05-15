57cdd73bda42f13fe22d94f40747fb78221761a8
John Leonardo <j@jdleo.me>
2021-05-15 17:50:31 -0500

    completed problem 701

--- solutions/701/main.go
+++ solutions/701/main.go
@@ -1,1 +1,24 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	// base case : found place to insert
+	if root == nil {
+		return &TreeNode{val, nil, nil}
+	}
+
+	// if less than current node, insert in left subtree, else insert right
+	if val < root.Val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+
+	return root
+}
