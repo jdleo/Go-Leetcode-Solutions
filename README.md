79aefb461bf0cb8ecc09aa1370f4275003dec683
John Leonardo <j@jdleo.me>
2021-05-15 00:25:35 -0500

    completed problem 804

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 122
+### # Of Problems Solved: 123
--- solutions/804/main.go
+++ solutions/804/main.go
@@ -1,1 +1,30 @@
 package main
+
+import "strings"
+
+func uniqueMorseRepresentations(words []string) int {
+	// mapping for char to morse
+	morse := []string{
+		".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+		"..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+		"--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+		"-.--", "--..",
+	}
+
+	// set for keeping track of words
+	set := map[string]struct{}{}
+
+	// go through words
+	for _, word := range words {
+		var cur strings.Builder
+		// go through character in word
+		for _, char := range word {
+			// add morse encoding to cur
+			cur.WriteString(morse[char-'a'])
+		}
+		// add current word to set
+		set[cur.String()] = struct{}{}
+	}
+
+	return len(set)
+}
