fb61824dc2131d40c179f22d292e3900fe6c2426
John Leonardo <j@jdleo.me>
2021-05-14 21:48:02 -0500

    completed problem 1266

--- solutions/1266/main.go
+++ solutions/1266/main.go
@@ -1,1 +1,29 @@
 package main
+
+func minTimeToVisitAllPoints(points [][]int) int {
+	res := 0
+
+	// go thru pointers
+	for i := 1; i < len(points); i++ {
+		// max of the delta between x and y coords
+		res += max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]))
+	}
+
+	return res
+}
+
+// helper methods below
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
