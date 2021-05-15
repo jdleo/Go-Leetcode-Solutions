1b0cdcd3785ffb783692b8ee507853373c0b830f
John Leonardo <j@jdleo.me>
2021-05-14 21:45:07 -0500

    completed problem 1816

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 119
+### # Of Problems Solved: 120
--- solutions/1816/main.go
+++ solutions/1816/main.go
@@ -1,1 +1,26 @@
 package main
+
+func truncateSentence(s string, k int) string {
+	res := []byte{}
+
+	// go through string
+	for _, char := range s {
+		// check if this is a space
+		if char == ' ' {
+			// decrease k
+			k--
+			// check if k is at 0 (we have all our words)
+			if k == 0 {
+				return string(res)
+			}
+			// push space
+			res = append(res, byte(char))
+		} else {
+			// push char
+			res = append(res, byte(char))
+		}
+	}
+
+	// return byte slice as string
+	return string(res)
+}
