aa68d5c14f7c8e7b2636af5f1c9133360ecf401b
John Leonardo <j@jdleo.me>
2021-05-15 13:02:14 -0500

    completed problem 1370

--- solutions/1370/main.go
+++ solutions/1370/main.go
@@ -3,10 +3,35 @@ package main
 func sortString(s string) string {
 	counts := make([]int, 26)
 	res := []byte{}
+	n := len(s)
 
 	// fill counts
 	for _, char := range s {
 		counts[char-'a']++
+	}
+
+	// go until we've picked all characters
+	for n > 0 {
+		// go through smallest characters (0 -> 25)
+		for i := 0; i < 26; i++ {
+			// check if we have a character to pick
+			if counts[i] > 0 {
+				// pick this character
+				res = append(res, byte('a'+i))
+				counts[i]--
+				n--
+			}
+		}
+		// go through largest characters (25 -> 0)
+		for i := 25; i > -1; i-- {
+			// check if we have a character to pick
+			if counts[i] > 0 {
+				// pick this character
+				res = append(res, byte('a'+i))
+				counts[i]--
+				n--
+			}
+		}
 	}
 
 	// return byte slice as string
