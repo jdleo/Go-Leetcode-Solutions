360fc2a30c39659c136ad84e6f9721ccb44e3326
John Leonardo <j@jdleo.me>
2021-05-15 13:30:49 -0500

    completed problem 1450

--- solutions/1450/main.go
+++ solutions/1450/main.go
@@ -1,1 +1,14 @@
 package main
+
+func busyStudent(startTime []int, endTime []int, queryTime int) int {
+	res := 0
+
+	for i := 0; i < len(startTime); i++ {
+		// check if query time lies within this student's interval
+		if startTime[i] <= queryTime && queryTime <= endTime[i] {
+			res++
+		}
+	}
+
+	return res
+}
