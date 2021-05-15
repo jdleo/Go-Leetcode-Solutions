b41a8f580ca0f434e3723ae4022f99c26f92ad4d
John Leonardo <j@jdleo.me>
2021-05-15 01:22:30 -0500

    completed problem 1817

--- solutions/1817/main.go
+++ solutions/1817/main.go
@@ -1,1 +1,25 @@
 package main
+
+func findingUsersActiveMinutes(logs [][]int, k int) []int {
+	res := make([]int, k)
+	// users map of id -> map of minutes
+	users := make(map[int]map[int]struct{})
+
+	// go through logs
+	for _, log := range logs {
+		if users[log[0]] == nil {
+			users[log[0]] = make(map[int]struct{})
+		}
+
+		// add this minute to user's map
+		users[log[0]][log[1]] = struct{}{}
+	}
+
+	// go through users
+	for _, uam := range users {
+		// add length of unique minutes (uam) to res
+		res[len(uam)-1]++
+	}
+
+	return res
+}
