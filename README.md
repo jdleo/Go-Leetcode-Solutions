370bf2806c5b1b95dde2e572fe5f6628bfcf9f9e
John Leonardo <j@jdleo.me>
2021-05-15 13:02:05 -0500

    completed problem 1370

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 134
+### # Of Problems Solved: 135
--- solutions/1370/main.go
+++ solutions/1370/main.go
@@ -1,1 +1,14 @@
 package main
+
+func sortString(s string) string {
+	counts := make([]int, 26)
+	res := []byte{}
+
+	// fill counts
+	for _, char := range s {
+		counts[char-'a']++
+	}
+
+	// return byte slice as string
+	return string(res)
+}
