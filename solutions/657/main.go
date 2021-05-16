8cfa2f3133206b5c2c2e5f580bf1eb0001c3de46
John Leonardo <j@jdleo.me>
2021-05-15 20:03:32 -0500

    completed problem 657

--- solutions/657/main.go
+++ solutions/657/main.go
@@ -1,1 +1,24 @@
 package main
+
+func judgeCircle(moves string) bool {
+	// x and y position
+	x, y := 0, 0
+
+	// go thru moves
+	for i := 0; i < len(moves); i++ {
+		// switch character
+		switch moves[i] {
+		case 'U':
+			y++
+		case 'D':
+			y--
+		case 'L':
+			x--
+		default:
+			x++
+		}
+	}
+
+	// check if back at origin
+	return x == 0 && y == 0
+}
