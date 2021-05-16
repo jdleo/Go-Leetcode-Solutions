6afd4f81d55946a48ece176be1e5279dac2b272f
John Leonardo <j@jdleo.me>
2021-05-15 19:17:55 -0500

    completed problem 1475

--- solutions/1475/main.go
+++ solutions/1475/main.go
@@ -1,1 +1,20 @@
 package main
+
+func finalPrices(prices []int) []int {
+	stack := []int{}
+
+	// go through prices
+	for i, price := range prices {
+		// check if we can use current price AS a discount for SOMETHING
+		for len(stack) > 0 && prices[stack[len(stack)-1]] >= price {
+			// apply discount, and pop from stack
+			prices[stack[len(stack)-1]] -= price
+			stack = stack[:len(stack)-1]
+		}
+		// add index to stack
+		stack = append(stack, i)
+	}
+
+	// discounted prices
+	return prices
+}
