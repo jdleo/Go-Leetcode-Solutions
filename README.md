ca73f793692dd702d5a73185452ac40740542c22
John Leonardo <j@jdleo.me>
2021-05-15 19:38:53 -0500

    completed problem 961

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 156
+### # Of Problems Solved: 157
--- solutions/961/main.go
+++ solutions/961/main.go
@@ -1,1 +1,11 @@
 package main
+
+func repeatedNTimes(nums []int) int {
+	for i := 2; i < len(nums); i++ {
+		// check if this num is equal to 1, or 2 back
+		if nums[i] == nums[i-1] || nums[i] == nums[i-2] {
+			return nums[i]
+		}
+	}
+	return nums[0]
+}
