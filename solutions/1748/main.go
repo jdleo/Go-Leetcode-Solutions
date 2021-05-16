f2cf0a645de4e390bec8a3c3eacaeeb4d86b0c5d
John Leonardo <j@jdleo.me>
2021-05-15 19:54:49 -0500

    completed problem 1748

--- solutions/1748/main.go
+++ solutions/1748/main.go
@@ -1,1 +1,19 @@
 package main
+
+func sumOfUnique(nums []int) int {
+	counts, res := map[int]int{}, 0
+
+	// go thru nums, and count individuals
+	for _, num := range nums {
+		counts[num]++
+	}
+
+	// go thru counts, and count those unique
+	for _, count := range counts {
+		if count == 1 {
+			res++
+		}
+	}
+
+	return res
+}
