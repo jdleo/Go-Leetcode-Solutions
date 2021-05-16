8018ea1dbe2af0c7c3a796df3b2efbdf23c90cb7
John Leonardo <j@jdleo.me>
2021-05-15 19:52:22 -0500

    completed problem 897

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 157
+### # Of Problems Solved: 158
--- solutions/897/main.go
+++ solutions/897/main.go
@@ -1,1 +1,37 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func increasingBST(root *TreeNode) *TreeNode {
+	head := &TreeNode{}
+	dummy := head
+
+	// stack for inorder
+	stack := []*TreeNode{}
+	// go while stack or root
+	for len(stack) > 0 || root != nil {
+		// check root
+		if root != nil {
+			// keep goin left
+			stack = append(stack, root)
+			root = root.Left
+		} else {
+			// pop from stack
+			root = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			// add to result (INORDER)
+			dummy.Right = root
+			root.Left = nil
+			dummy = dummy.Right
+			// go right
+			root = root.Right
+		}
+	}
+
+	return head.Right
+}
