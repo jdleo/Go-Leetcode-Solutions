5cfc96ad5214288a63c757c222dbffbd2f026923
John Leonardo <j@jdleo.me>
2021-05-16 12:21:38 -0500

    completed problem 359

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 166
+### # Of Problems Solved: 167
--- solutions/359/main.go
+++ solutions/359/main.go
@@ -1,1 +1,32 @@
 package main
+
+type Logger struct {
+	logs map[string]int // map to map message->timestamp
+}
+
+/** Initialize your data structure here. */
+func Constructor() Logger {
+	return Logger{map[string]int{}}
+}
+
+/** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+  If this method returns false, the message will not be printed.
+  The timestamp is in seconds granularity. */
+func (this *Logger) ShouldPrintMessage(timestamp int, message string) bool {
+	// check if this is even in the map
+	if t, ok := this.logs[message]; ok {
+		// check if t was closer than 10 seconds ago
+		if timestamp-t < 10 {
+			// can't create
+			return false
+		} else {
+			// can create
+			this.logs[message] = timestamp
+			return true
+		}
+	} else {
+		// create it
+		this.logs[message] = timestamp
+		return true
+	}
+}
