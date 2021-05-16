1491558c5dabdeb867aafade0f056bc2f1503921
John Leonardo <j@jdleo.me>
2021-05-15 20:30:12 -0500

    completed problem 942

--- solutions/942/main.go
+++ solutions/942/main.go
@@ -1,1 +1,22 @@
 package main
+
+func diStringMatch(s string) []int {
+	left, right := 0, len(s)
+	res := make([]int, len(s)+1)
+
+	// go thru string
+	for i, char := range s {
+		// check if increasing
+		if char == 'I' {
+			res[i] = left
+			left++
+		} else {
+			res[i] = right
+			right--
+		}
+	}
+
+	// set the last one (can be either)
+	res[len(s)] = left
+	return res
+}
