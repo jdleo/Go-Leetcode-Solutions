80a5bea11310954918428782b2ca5e2cd33769f5
John Leonardo <j@jdleo.me>
2021-05-16 12:30:29 -0500

    completed problem 419

--- solutions/419/main.go
+++ solutions/419/main.go
@@ -1,1 +1,37 @@
 package main
+
+func countBattleships(board [][]byte) int {
+	res, m, n := 0, len(board), len(board[0])
+
+	// recursive, dfs helper method to sink battleships
+	var sink func(i, j int)
+	sink = func(i, j int) {
+		// base case : out of bounds, or not battleship
+		if i < 0 || i == m || j < 0 || j == n || board[i][j] != 'X' {
+			return
+		}
+
+		// sink
+		board[i][j] = '.'
+
+		// go up,down,right,left
+		sink(i+1, j)
+		sink(i-1, j)
+		sink(i, j+1)
+		sink(i, j-1)
+	}
+
+	// go thru grid
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			// check if battle ship
+			if board[i][j] == 'X' {
+				// sink
+				sink(i, j)
+				res++
+			}
+		}
+	}
+
+	return res
+}
