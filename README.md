d03f9bad538961e8149769bbdebc583aa9207191
John Leonardo <j@jdleo.me>
2021-05-16 23:29:20 -0500

    completed problem 366

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 173
+### # Of Problems Solved: 174
--- solutions/366/main.go
+++ solutions/366/main.go
@@ -1,1 +1,40 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findLeaves(root *TreeNode) [][]int {
+	res := [][]int{}
+	helper(root, &res)
+	return res
+}
+
+// recursive helper function
+func helper(root *TreeNode, res *[][]int) int {
+	if root == nil {
+		return 0
+	}
+
+	left := helper(root.Left, res)
+	right := helper(root.Right, res)
+	level := max(left, right) + 1
+
+	if level > len(*res) {
+		*res = append(*res, []int{})
+	}
+
+	(*res)[level-1] = append((*res)[level-1], root.Val)
+
+	return level
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
