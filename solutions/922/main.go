06e2d7a283a07f60398cd94c964e9d03b65a3982
john leonardo <j@jdleo.me>
2021-05-17 15:31:24 -0500

    completed problem 922

--- solutions/922/main.go
+++ solutions/922/main.go
@@ -1,1 +1,17 @@
 package main
+
+func sortArrayByParityII(nums []int) []int {
+	left, right := 0, 1
+
+	for left < len(nums) {
+		// check if left is out of place
+		if nums[left]&1 == 1 {
+			// swap with whats at right, and move right forward
+			nums[left], nums[right], right = nums[right], nums[left], right+2
+		} else {
+			left += 2
+		}
+	}
+
+	return nums
+}
