e77145332b516ea547e0cd49efd14c2d2b127e60
John Leonardo <j@jdleo.me>
2021-05-16 23:45:28 -0500

    completed problem 811

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 175
+### # Of Problems Solved: 176
--- solutions/811/main.go
+++ solutions/811/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func subdomainVisits(cpdomains []string) []string {
+	res := []string{}
+	// map of each subdomain to its count
+	counts := map[string]int{}
+
+	// go through domains
+	for _, domain := range cpdomains {
+		// get visits, domain using helper func
+		visits, d := parse(domain)
+		// get subdomains
+		subs := strings.Split(d, ".")
+		// get d
+		d = subs[len(subs)-1]
+		// go through length
+		for i := len(subs) - 2; i > -1; i-- {
+			counts[d] += visits
+			d = subs[i] + "." + d
+		}
+		counts[d] += visits
+	}
+
+	// go through counts
+	for domain, count := range counts {
+		// add formatted string to res
+		res = append(res, fmt.Sprintf("%d %s", count, domain))
+	}
+
+	return res
+}
+
+func parse(domain string) (int, string) {
+	res := strings.Split(domain, " ")
+	visits, _ := strconv.Atoi(res[0])
+	return visits, res[1]
+}
