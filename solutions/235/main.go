01e90cd0cf67f864013372a38fc2f32cf30efc28
John Leonardo <j@jdleo.me>
2021-05-17 18:58:38 -0500

    completed problem 235

--- solutions/235/main.go
+++ solutions/235/main.go
@@ -1,1 +1,36 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	// check if the max of p q are smaller than root
+	if max(p.Val, q.Val) < root.Val {
+		// we need to search left
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if min(p.Val, q.Val) > root.Val {
+		// we need to search right
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		// THIS is the LCA
+		return root
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
