4e3cc8fc96a07d9c868b22e195dba5aefbbefe31
John Leonardo <j@jdleo.me>
2021-05-16 22:55:59 -0500

    create problem 933

--- solutions/1472/main.go
+++ solutions/1472/main.go
@@ -1,1 +1,26 @@
 package main
+
+type BrowserHistory struct {
+	homepage string
+	front    []string
+	back     []string
+}
+
+func Constructor(homepage string) BrowserHistory {
+	return BrowserHistory{homepage, []string{}, []string{}}
+}
+
+func (this *BrowserHistory) Visit(url string) {
+	// clear backwards history
+	this.back = []string{}
+	// push to front
+	this.front = append(this.front, url)
+}
+
+func (this *BrowserHistory) Back(steps int) string {
+
+}
+
+func (this *BrowserHistory) Forward(steps int) string {
+
+}
