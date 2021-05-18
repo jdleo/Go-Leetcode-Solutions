26469e68a13f5d5ab1be380c1225748c531211d8
john leonardo <j@jdleo.me>
2021-05-18 15:47:24 -0500

    create problem 572

--- solutions/516/main.go
+++ solutions/516/main.go
@@ -1,1 +1,19 @@
 package main
+
+func longestPalindromeSubseq(s string) int {
+	res := 0
+
+	return res
+}
+
+// helper method to expand on a palindrome
+func expand(s string, i, j int) int {
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
