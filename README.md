029727af9c99c5efc653383a1a4a3ebf76693cac
John Leonardo <j@jdleo.me>
2021-05-17 19:23:59 -0500

    completed problem 424

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 190
+### # Of Problems Solved: 191
--- solutions/424/main.go
+++ solutions/424/main.go
@@ -1,1 +1,30 @@
 package main
+
+func characterReplacement(s string, k int) int {
+	counts := make([]int, 26)
+	maxCount, maxLength := 0, 0
+
+	for start, end := 0, 0; end < len(s); end++ {
+		// increment count for this character
+		counts[s[end]-'A']++
+		// set new max count
+		maxCount = max(maxCount, counts[s[end]-'A'])
+		// while the rest of characters - max count > k
+		for end-start+1-maxCount > k {
+			// remove from start n increment it
+			counts[s[start]-'A']--
+			start++
+		}
+		// set new max length
+		maxLength = max(maxLength, end-start+1)
+	}
+
+	return maxLength
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
