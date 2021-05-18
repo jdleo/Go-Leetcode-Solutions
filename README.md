fdde3465bef562edf527ad80b52ab34c0031d757
john leonardo <j@jdleo.me>
2021-05-18 15:52:45 -0500

    completed problem 572

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 189
+### # Of Problems Solved: 191
--- solutions/572/main.go
+++ solutions/572/main.go
@@ -1,1 +1,38 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil || subRoot == nil {
+		return root == subRoot
+	}
+
+	// check if we found potential subroot
+	if root.Val == subRoot.Val {
+		if isSame(root, subRoot) {
+			return true
+		}
+	}
+
+	// search left and right for root of subroot
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+// recursive helper function to determine if two trees are same
+func isSame(a *TreeNode, b *TreeNode) bool {
+	if a == nil || b == nil {
+		return a == b
+	}
+
+	// make sure these are the same
+	if a.Val != b.Val {
+		return false
+	}
+
+	return isSame(a.Left, b.Left) && isSame(a.Right, b.Right)
+}
