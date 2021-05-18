4138181b0c6a48d2bab317ac8243b6ccb9c0c387
John Leonardo <j@jdleo.me>
2021-05-17 19:34:12 -0500

    completed problem 253

--- solutions/253/main.go
+++ solutions/253/main.go
@@ -1,1 +1,29 @@
 package main
+
+import "sort"
+
+func minMeetingRooms(intervals [][]int) int {
+	var points []point
+	for _, interval := range intervals {
+		points = append(points, point{interval[0], true}, point{interval[1], false})
+	}
+	sort.Slice(points, func(i, j int) bool {
+		if points[i].time == points[j].time {
+			return !points[i].isStart
+		}
+		return points[i].time < points[j].time
+	})
+
+	var rooms, max int
+	for _, pt := range points {
+		if pt.isStart {
+			rooms++
+		} else {
+			rooms--
+		}
+		if rooms > max {
+			max = rooms
+		}
+	}
+	return max
+}
