7710d307990159132d94edaf6129792683204247
john leonardo <j@jdleo.me>
2021-05-19 16:13:26 -0500

    completed problem 1265

--- solutions/1265/main.go
+++ solutions/1265/main.go
@@ -1,1 +1,25 @@
 package main
+
+// Below is the interface for ImmutableListNode, which is already defined for you.
+type ImmutableListNode struct {
+}
+
+func (this *ImmutableListNode) getNext() ImmutableListNode {
+	// return the next node.
+	return ImmutableListNode{}
+}
+
+func (this *ImmutableListNode) printValue() {
+	// print the value of this node.
+}
+
+func printLinkedListInReverse(head ImmutableListNode) {
+	// base case
+	if head == nil {
+		return
+	}
+	// dfs to the bottom first
+	printLinkedListInReverse(head.getNext())
+	// now print
+	head.printValue()
+}
