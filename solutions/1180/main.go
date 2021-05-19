498cee9300bf9a0f49a01ba06b1178c7a3785b44
john leonardo <j@jdleo.me>
2021-05-19 16:21:25 -0500

    completed problem 1180

--- solutions/1180/main.go
+++ solutions/1180/main.go
@@ -1,1 +1,21 @@
 package main
+
+func countLetters(s string) int {
+	res := 0
+	same := 1 // initialize to one, since first character we see is valid
+
+	for i, char := range s {
+		if i > 0 {
+			if s[i-1] == byte(char) {
+				same++
+			} else {
+				same = 1
+			}
+		}
+
+		// add unique substring to res
+		res += same
+	}
+
+	return res
+}
