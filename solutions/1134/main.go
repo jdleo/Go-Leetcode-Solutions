11f7f71380bf0ae7172021b5ffa96fdddc398cd1
John Leonardo <j@jdleo.me>
2021-05-19 21:14:19 -0500

    completed problem 1134

--- solutions/1134/main.go
+++ solutions/1134/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"math"
+)
+
+func isArmstrong(n int) bool {
+	k := math.Floor(math.Log10(float64(n)) + 1)
+	res, x := 0.0, n
+
+	for x > 0 {
+		// add rightmost digit to the power of k
+		res += math.Pow(float64(x%10), k)
+		// move digit over
+		x /= 10
+	}
+
+	// check if sum of powers is equal to n
+	return res == float64(n)
+}
