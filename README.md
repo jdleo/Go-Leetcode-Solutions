da747962c72fabef3d8ee52d9e87f7dfedc515ff
john leonardo <j@jdleo.me>
2021-05-20 16:10:09 -0500

    completed problem 977

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 217
+### # Of Problems Solved: 218
--- solutions/977/main.go
+++ solutions/977/main.go
@@ -1,1 +1,24 @@
 package main
+
+func sortedSquares(nums []int) []int {
+	res := make([]int, len(nums))
+
+	// go until left and right meet
+	for i, left, right := 0, 0, len(nums)-1; left <= right; i++ {
+		// take bigger number squared
+		if nums[left]*nums[left] > nums[right]*nums[right] {
+			res[i] = nums[left] * nums[left]
+			left++
+		} else {
+			res[i] = nums[right] * nums[right]
+			right--
+		}
+	}
+
+	// reverse res
+	for left, right := 0, len(nums)-1; left < right; left, right = left+1, right-1 {
+		res[left], res[right] = res[right], res[left]
+	}
+
+	return res
+}
