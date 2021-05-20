64ca4a88500bb7151c96ef456faaac224470b3f1
John Leonardo <j@jdleo.me>
2021-05-19 20:03:32 -0500

    completed problem 346

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 207
+### # Of Problems Solved: 208
--- solutions/346/main.go
+++ solutions/346/main.go
@@ -1,1 +1,29 @@
 package main
+
+type MovingAverage struct {
+	queue []int
+	sum   int
+	size  int
+}
+
+/** Initialize your data structure here. */
+func Constructor(size int) MovingAverage {
+	return MovingAverage{[]int{}, 0, size}
+}
+
+func (this *MovingAverage) Next(val int) float64 {
+	// add val to queue, and sum
+	this.queue = append(this.queue, val)
+	this.sum += val
+
+	// check if queue exceeds size
+	if len(this.queue) > this.size {
+		// popleft, and take sum out
+		first := this.queue[0]
+		this.queue = this.queue[1:]
+		this.sum -= first
+	}
+
+	// return sum / size of queue
+	return float64(this.sum) / float64(len(this.queue))
+}
