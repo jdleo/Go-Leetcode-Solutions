a7ca4603fd48e0f9f678c5948a6e46ce94e5557e
John Leonardo <j@jdleo.me>
2021-05-19 22:48:35 -0500

    completed problem 1166

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 213
+### # Of Problems Solved: 214
--- solutions/1166/main.go
+++ solutions/1166/main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"strings"
+)
+
+type FileSystem struct {
+	Value    int
+	Children map[string]FileSystem
+}
+
+func Constructor() FileSystem {
+	return FileSystem{-1, map[string]FileSystem{}}
+}
+
+func (this *FileSystem) CreatePath(path string, value int) bool {
+	fullPath := strings.Split(path, "/")
+	cur := *this
+
+	for i := 1; i < len(fullPath)-1; i++ {
+		// check if current level is in this children
+		if v, ok := cur.Children[fullPath[i]]; ok {
+			cur = v
+		} else {
+			// can't create
+			return false
+		}
+	}
+
+	// no issues, and create it, if it doesn't exist
+	if _, ok := cur.Children[fullPath[len(fullPath)-1]]; ok {
+		return false
+	} else {
+		cur.Children[fullPath[len(fullPath)-1]] = FileSystem{value, map[string]FileSystem{}}
+		return true
+	}
+}
+
+func (this *FileSystem) Get(path string) int {
+	fullPath := strings.Split(path, "/")
+	cur := *this
+
+	// traverse through path
+	for i := 1; i < len(fullPath); i++ {
+		// check if current level is in this children
+		if v, ok := cur.Children[fullPath[i]]; ok {
+			cur = v
+		} else {
+			// not found
+			return -1
+		}
+	}
+
+	// return cur's value
+	return cur.Value
+}
