ddf5c160ea46df5661f468c6822353f4db434a83
John Leonardo <j@jdleo.me>
2021-05-22 17:56:00 -0500

    completed problem 766

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 226
+### # Of Problems Solved: 227
--- solutions/766/main.go
+++ solutions/766/main.go
@@ -1,1 +1,25 @@
 package main
+
+func isToeplitzMatrix(matrix [][]int) bool {
+	// map to hold diagonal values
+	diag := map[int]int{}
+
+	// go thru matrix
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[i]); j++ {
+			// check if diagonal exists
+			if v, ok := diag[i-j]; ok {
+				// make sure it matches value
+				if matrix[i][j] != v {
+					return false
+				}
+			} else {
+				// store it
+				diag[i-j] = matrix[i][j]
+			}
+		}
+	}
+
+	// no issues encountered
+	return true
+}
