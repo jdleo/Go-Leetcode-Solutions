89579f1de2649ad3cf13e05bdc9087dcb30152d7
John Leonardo <j@jdleo.me>
2021-05-22 18:03:49 -0500

    completed problem 791

--- solutions/791/main.go
+++ solutions/791/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"sort"
+)
+
+func customSortString(order string, str string) string {
+	// make str a byte slice
+	res := []byte(str)
+
+	// fill characters of order, based on where they were seen in order
+	index := make([]int, 26)
+	for i, char := range order {
+		index[char-'a'] = i
+	}
+
+	// sort result string based on index mapping
+	sort.Slice(res, func(i, j int) bool {
+		return index[res[i]-'a'] < index[res[j]-'a']
+	})
+
+	return string(res)
+}
