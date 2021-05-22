82b0433764cf7a95511f2382044c977d7bc0a76c
John Leonardo <j@jdleo.me>
2021-05-22 18:14:17 -0500

    completed problem 695

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 229
+### # Of Problems Solved: 230
--- solutions/695/main.go
+++ solutions/695/main.go
@@ -1,1 +1,38 @@
 package main
+
+func maxAreaOfIsland(grid [][]int) int {
+	res := 0
+
+	// go thru grid
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			// set new max area, if need be, and sink island
+			area := sinkAndGetArea(grid, i, j)
+			if area > res {
+				res = area
+			}
+		}
+	}
+
+	return res
+}
+
+// helper method to 'sink' island, and return area of it
+func sinkAndGetArea(grid [][]int, i, j int) int {
+	// bounds check
+	if i < 0 || i == len(grid) || j < 0 || j == len(grid[i]) || grid[i][j] != 1 {
+		return 0
+	}
+
+	// sink island, and count for this area
+	area := 1
+	grid[i][j] = 0
+
+	// all possible directions
+	area += sinkAndGetArea(grid, i+1, j)
+	area += sinkAndGetArea(grid, i-1, j)
+	area += sinkAndGetArea(grid, i, j+1)
+	area += sinkAndGetArea(grid, i, j-1)
+
+	return area
+}
