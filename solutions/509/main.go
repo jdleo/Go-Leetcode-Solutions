1bc2bd14a4bd1b9804607e6ce3f0389133c43845
John Leonardo <j@jdleo.me>
2021-05-22 17:33:06 -0500

    completed problem 509

--- solutions/509/main.go
+++ solutions/509/main.go
@@ -1,1 +1,18 @@
 package main
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	// this represents fib(n-1) and fib(n-2)
+	a, b := 1, 0
+
+	// calculate 2 to n
+	for i := 2; i <= n; i++ {
+		// fib(n-1) + fib(n-2)
+		a, b = a+b, a
+	}
+
+	return a
+}
