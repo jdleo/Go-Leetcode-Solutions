8b3e5badf365b519b81936ccfb7f4cf40155495d
John Leonardo <j@jdleo.me>
2021-05-22 18:08:25 -0500

    completed problem 349

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 228
+### # Of Problems Solved: 229
--- solutions/349/main.go
+++ solutions/349/main.go
@@ -1,1 +1,23 @@
 package main
+
+func intersection(nums1 []int, nums2 []int) []int {
+	res := []int{}
+	seen := map[int]bool{}
+
+	// mark nums as seen
+	for _, num := range nums1 {
+		seen[num] = true
+	}
+
+	// go thru nums2
+	for _, num := range nums2 {
+		// check if we've seen this number
+		if seen[num] {
+			// add to res, and mark as not seen to ensure uniqueness
+			res = append(res, num)
+			seen[num] = false
+		}
+	}
+
+	return res
+}
