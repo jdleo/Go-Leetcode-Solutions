728d1402279d48bdf97b1f8c911707dbed063635
John Leonardo <j@jdleo.me>
2021-05-22 17:27:09 -0500

    completed problem 406

--- solutions/406/main.go
+++ solutions/406/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"sort"
+)
+
+func reconstructQueue(people [][]int) [][]int {
+	// sort by heights descending, queue position ascending
+	sort.Slice(people, func(i, j int) bool {
+		// if equal heights, sort queue position
+		if people[i][0] == people[j][0] {
+			return people[i][1] < people[j][1]
+		}
+		// sort by height
+		return people[i][0] > people[j][0]
+	})
+
+	res := make([][]int, len(people))
+
+	// go thru sorted people
+	for _, person := range people {
+		// get index to insert at, and insert at index
+		index := person[1]
+		res = append(res[:index+1], res[index:]...)
+		res[index] = person
+	}
+
+	return res[:len(people)]
+}
