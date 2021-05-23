d364bf7ca4c80cc36e16b3453f9c1b3c565bc3a3
John Leonardo <j@jdleo.me>
2021-05-23 13:23:28 -0500

    completed problem 463

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 231
+### # Of Problems Solved: 232
--- solutions/463/main.go
+++ solutions/463/main.go
@@ -1,1 +1,27 @@
 package main
+
+func islandPerimeter(grid [][]int) int {
+	res := 0
+
+	// go thru grid
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			// check if this is an island
+			if grid[i][j] == 1 {
+				res += 4
+
+				// check if left was already counted
+				if j > 0 && grid[i][j-1] == 1 {
+					res -= 2
+				}
+
+				// check if top was already counted
+				if i > 0 && grid[i-1][j] == 1 {
+					res -= 2
+				}
+			}
+		}
+	}
+
+	return res
+}
