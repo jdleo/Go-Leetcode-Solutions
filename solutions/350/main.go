05d7f34a151ddcbf80e368fe0d886ae7479f6f4d
John Leonardo <j@jdleo.me>
2021-05-23 13:25:11 -0500

    completed problem 350

--- solutions/350/main.go
+++ solutions/350/main.go
@@ -1,1 +1,25 @@
 package main
+
+func intersect(nums1 []int, nums2 []int) []int {
+	// counts of frequencies
+	counts := map[int]int{}
+
+	// fill counts
+	for _, num := range nums1 {
+		counts[num]++
+	}
+
+	res := []int{}
+
+	// go thru nums2
+	for _, num := range nums2 {
+		// check if still numbers to take
+		if counts[num] > 0 {
+			// take, decrement count
+			res = append(res, num)
+			counts[num]--
+		}
+	}
+
+	return res
+}
