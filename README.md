da4fe3753fac2a65534c28860f2fdee7d10b0c0e
john leonardo <j@jdleo.me>
2021-06-10 09:53:36 -0500

    completed problem 234

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 244
+### # Of Problems Solved: 245
--- solutions/234/main.go
+++ solutions/234/main.go
@@ -1,1 +1,39 @@
 package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	slow, fast := head, head
+
+	// first, find the middle node
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+
+	// next, reverse the second half
+	var prev *ListNode = nil
+	for slow != nil {
+		// 1 -> 2 -> 3
+		// null <- 1
+		next := slow.Next
+		slow.Next = prev
+		prev = slow
+		slow = next
+	}
+
+	// lastly, compare head with second half
+	for prev != nil {
+		// compare with prev (which is head of second half)
+		if head.Val != prev.Val {
+			return false
+		}
+		prev, head = prev.Next, head.Next
+	}
+
+	// no issues
+	return true
+}
