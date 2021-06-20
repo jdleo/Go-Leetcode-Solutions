c97b1cb3a43bac082f194c85d8de0cb852256734
John Leonardo <j@jdleo.me>
2021-06-19 20:42:10 -0500

    completed problem 112

--- solutions/112/main.go
+++ solutions/112/main.go
@@ -1,1 +1,22 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	targetSum -= root.Val
+
+	if root.Left == nil && root.Right == nil && targetSum == 0 {
+		return true
+	}
+
+	return hasPathSum(root.Left, targetSum) || hasPathSum(root.Right, targetSum)
+}
