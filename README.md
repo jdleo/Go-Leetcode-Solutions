33cd82785908ec1e124c1779433de3a5c1f56620
john leonardo <j@jdleo.me>
2021-06-20 22:40:29 -0500

    completed problem 654

--- README.md
+++ README.md
@@ -19,4 +19,4 @@ To mark a problem as complete:
 ``` 
 
 
-### # Of Problems Solved: 256
+### # Of Problems Solved: 257
--- solutions/654/main.go
+++ solutions/654/main.go
@@ -1,1 +1,37 @@
 package main
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	// call recursive builder
+	return build(nums, 0, len(nums)-1)
+}
+
+func build(nums []int, left, right int) *TreeNode {
+	// base case : left and right crossover (no nums to search)
+	if left > right {
+		return nil
+	}
+
+	// find max index in nums
+	maxIdx := left
+	for i := maxIdx; i <= right; i++ {
+		if nums[i] > nums[maxIdx] {
+			maxIdx = i
+		}
+	}
+
+	// make new root with num at max index
+	root := &TreeNode{nums[maxIdx], nil, nil}
+
+	// set left and right
+	root.Left = build(nums, left, maxIdx-1)
+	root.Right = build(nums, maxIdx+1, right)
+
+	return root
+}
